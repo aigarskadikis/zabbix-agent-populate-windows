75cc4176c145088f2e0c6ecc4ead0ed0c4fe6a8c
catonrug <aigars.kadikis@gmail.com>
2020-06-23 13:36:59 +0300

    next

--- populate.cmd
+++ populate.cmd
@@ -1,5 +1,7 @@
 @echo off
 
+set version=5.0.1
+
 net session 1>NUL 2>NUL || (Echo Please run this script as administrator & Exit /b 1)
 
 rem set where the zabbix agent will be deployed 'c:\zabbix' is more recommended
@@ -46,6 +48,36 @@ if not !errorlevel!==0 echo Cannot start Zabbix Agent service. Result code !erro
 
 ) else echo Cannot install Zabbix Agent. Result code !errorlevel!
 ) else echo Cannot create "%zabbix%". Result code !errorlevel!
-) else echo Zabbix Agent already exist
+) 
+
+rem check the version
+"%zabbix%\zabbix_agentd.exe" -V | find "%version%"> nul 2>&1
+if not !errorlevel!==0 (
+
+echo No "%version%" version detected, but
+"%zabbix%\zabbix_agentd.exe" -V | find "zabbix_agentd"
+
+echo.
+echo Stopping agent..
+sc stop "Zabbix Agent"
+
+rem install zabbix_agentd.exe, zabbix_sender.exe, zabbix_get for 32-bit system
+if not exist "%programfiles(x86)%" (
+echo this is 32-bit system
+xcopy /Y "%~dp0win32\*" "%zabbix%"
+)
+
+rem install zabbix_agentd.exe, zabbix_sender.exe, zabbix_get for 64-bit system
+if exist "%programfiles(x86)%" (
+echo this is 64-bit system
+xcopy /Y "%~dp0win64\*" "%zabbix%"
+)
+
+echo.
+echo Starting agent..
+
+sc start "Zabbix Agent"
+
+) else echo Version %version% already exist
 
 endlocal
